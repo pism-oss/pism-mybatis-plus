d1919c2dc7b4e02d335538927a9d5346fb51dd72
PerccyKing <perccyking@gmail.com>
2026-03-16 22:55:00 +0800

    feat 删除EntityFill 标记

--- pism-mybatis-plus-core/src/main/java/cn/com/pism/mybatis/core/service/impl/ComServiceImpl.java
+++ pism-mybatis-plus-core/src/main/java/cn/com/pism/mybatis/core/service/impl/ComServiceImpl.java
@@ -4,7 +4,6 @@ import cn.com.pism.mybatis.core.annotation.EntityFill;
 import cn.com.pism.mybatis.core.mapper.ComMapper;
 import cn.com.pism.mybatis.core.service.ComService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
@@ -66,109 +65,4 @@ public class ComServiceImpl<M extends ComMapper<T>, T> extends ServiceImpl<M, T>
         return baseMapper.updateSelectiveByPrimaryKey(entity);
     }
 
-
-    /**
-     * 插入一条记录（选择字段，策略插入）
-     *
-     * @param entity 实体对象
-     */
-    @Override
-    @EntityFill
-    public boolean save(T entity) {
-        return super.save(entity);
-    }
-
-    /**
-     * 插入（批量）
-     *
-     * @param entityList 实体对象集合
-     */
-    @Transactional(rollbackFor = Exception.class)
-    @Override
-    @EntityFill
-    public boolean saveBatch(Collection<T> entityList) {
-        return saveBatch(entityList, DEFAULT_BATCH_SIZE);
-    }
-
-    /**
-     * 插入（批量）
-     *
-     * @param entityList 实体对象集合
-     * @param batchSize  插入批次数量
-     */
-    @EntityFill
-    @Override
-    public boolean saveBatch(Collection<T> entityList, int batchSize) {
-        return super.saveBatch(entityList, batchSize);
-    }
-
-    /**
-     * 批量修改插入
-     *
-     * @param entityList 实体对象集合
-     */
-    @Transactional(rollbackFor = Exception.class)
-    @EntityFill
-    @Override
-    public boolean saveOrUpdateBatch(Collection<T> entityList) {
-        return super.saveOrUpdateBatch(entityList);
-    }
-
-    /**
-     * 批量修改插入
-     *
-     * @param entityList 实体对象集合
-     * @param batchSize  每次的数量
-     */
-    @Override
-    @EntityFill
-    public boolean saveOrUpdateBatch(Collection<T> entityList, int batchSize) {
-        return super.saveOrUpdateBatch(entityList, batchSize);
-    }
-
-    /**
-     * 根据 ID 选择修改
-     *
-     * @param entity 实体对象
-     */
-    @Override
-    @EntityFill
-    public boolean updateById(T entity) {
-        return super.updateById(entity);
-    }
-
-    /**
-     * 根据ID 批量更新
-     *
-     * @param entityList 实体对象集合
-     */
-    @Transactional(rollbackFor = Exception.class)
-    @Override
-    @EntityFill
-    public boolean updateBatchById(Collection<T> entityList) {
-        return super.updateBatchById(entityList);
-    }
-
-    /**
-     * 根据ID 批量更新
-     *
-     * @param entityList 实体对象集合
-     * @param batchSize  更新批次数量
-     */
-    @Override
-    @EntityFill
-    public boolean updateBatchById(Collection<T> entityList, int batchSize) {
-        return super.updateBatchById(entityList, batchSize);
-    }
-
-    /**
-     * TableId 注解存在更新记录，否插入一条记录
-     *
-     * @param entity 实体对象
-     */
-    @Override
-    @EntityFill
-    public boolean saveOrUpdate(T entity) {
-        return super.saveOrUpdate(entity);
-    }
 }
